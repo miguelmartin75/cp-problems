35f55215c0797c77e4dd0094442e8ae027c50d8f
Miguel Martin <miguel@miguel-martin.com>
2017-01-27 01:25:36 +1030

    Add script to count problems solved

---
+++ count.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function count()
+{
+    find $1 -name "*" -type f | wc -l
+}
+
+function print_table()
+{
+    dirs=(codeforces topcoder uva)
+
+    echo "| SITE.| AMOUNT SOLVED"
+    echo "|-----------.|---------------"
+    total=0
+    for d in ${dirs[@]}
+    do
+        c=$(count $d)
+        echo "| $d.| $c"
+        total=$((total+$c))
+    done
+
+    echo "|-----------.|---------------"
+    echo "| TOTAL.| $total"
+}
+
+print_table | column -t -s '.'
