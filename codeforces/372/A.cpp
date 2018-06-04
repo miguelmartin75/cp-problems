61e353f9a99aeb1bb5a6529e7102e3e40a23d23e
Miguel Martin <miguel@miguel-martin.com>
2018-06-04 22:35:10 +0930

    372A - simplify implementation

--- codeforces/372/A.cpp
+++ codeforces/372/A.cpp
@@ -15,16 +15,14 @@ int main(int argc, char *argv[])
     int p1 = A.size() / 2 - 1;
 
     int count = 0;
-    vector<bool> occ(n);
     while(p1 >= 0 && p2 >= 0)
     {
-        while(p1 >= 0 && (A[p2] < 2*A[p1] || occ[p1])) { --p1; }
+        while(p1 >= 0 && A[p2] < 2*A[p1]) { --p1; }
         if(p1 >= 0)
         {
             count++;
-            occ[p1] = occ[p2] = true;
-            while(p1 >= 0 && occ[p1]) p1--;
-            while(p2 >= 0 && occ[p2]) p2--;
+            p1--;
+            p2--;
         }
         else break;
     }
